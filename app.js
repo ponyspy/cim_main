f3e36b7db4c7f5c42a4b01903959c036e232aaa2
ponyspy <desade4me@gmail.com>
2013-07-04 09:08:32 +0400

    many changes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var fs = require('fs');
 var express = require('express');
     var app = express();
 
-// var mongoose = require('mongoose');
-  // var Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+  var Schema = mongoose.Schema;
+mongoose.connect('localhost', 'main');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
@@ -25,39 +26,52 @@ app.use(app.router);
 // -------------------
 
 
-// mongoose.connect('localhost', 'main');
+var eventSchema = new Schema({
+   _parent: { type: Schema.Types.ObjectId, ref: 'Event' },
+   _locale: [{ type: Schema.Types.ObjectId, ref: 'Locale' }],
+       img: {
+          path: String,
+        author: String
+       },
+       tag: String,
+      date: {type: Date, default: Date.now},
+  children: [{ type: Schema.Types.ObjectId, ref: 'Event' }]
+});
 
-// var orderSchema = new Schema({
-//   user_id: String,
-//   course_id: String,
-//   course_date: String,
-//   status: {type: String, default: 'Process'},
-//   date: {type: Date, default: Date.now},
-// });
+var localeSchema = new Schema({
+    locale: {type: String, default: 'RU'},
+     title: String,
+   s_title: String,
+      body: String,
+  director: String,
+  art_director: String,
+      hall: String,
+    actors: [String]
+});
 
-// var courseSchema = new Schema({
-//   title: String,
-//   description: String,
-//   price: Number,
-//   cathegory: String,
-//   schedule: [String],
-//   date: {type: Date, default: Date.now},
-// });
+var userSchema = new Schema({
+   login: String,
+    pass: String,
+  status: {type: String, default: 'User'},
+    date: {type: Date, default: Date.now},
+});
 
-// var UserSchema = new Schema({
-//   name: String,
-//   login: String,
-//   pass: String,
-//   email: String,
-//   skype: String,
-//   status: {type: String, default: 'User'},
-//   date: {type: Date, default: Date.now},
-//   items: [courseSchema]
-// });
+var User = mongoose.model('User', userSchema);
+var Event = mongoose.model('Event', eventSchema);
+var Locale = mongoose.model('Locale', localeSchema);
 
-// var Order = mongoose.model('Order', orderSchema);
-// var User = mongoose.model('User', UserSchema);
-// var Course = mongoose.model('Item', courseSchema);
+
+// ------------------------
+// *** Midleware Block ***
+// ------------------------
+
+
+function checkAuth (req, res, next) {
+  if (req.session.user_id)
+    next();
+  else
+    res.redirect('/login');
+}
 
 
 // ------------------------
@@ -103,6 +117,119 @@ app.post('/', function (req, res) {
   else
     res.send(arr.slice(post.offset, post.offset*2 || 6));  
 });
+
+
+app.get('/event', function (req, res) {
+  Event.find().populate({ path: '_locale', match: { locale: 'RU' }}).exec(function(err, event) {
+    res.render('event', {event: event});
+  });
+});
+
+app.get('/auth', checkAuth, function (req, res) {
+  res.render('auth');
+});
+
+app.post('/auth', checkAuth, function (req, res) {
+  console.log(req.body);
+  res.redirect('back');
+});
+
+app.get('/auth/add', checkAuth, function (req, res) {
+  res.render('add');
+});
+
+app.post('/auth/add', function(req, res) {
+  var post = req.body;
+  var user = req.session.user;
+  var pass = req.session.pass;
+
+  var event = new Event ({
+    tag: post.tag,
+  });
+
+  var locale = new Locale ({
+    title: post.title,
+    body: post.body
+  });
+
+
+  event.save(function(err, event) {
+    locale.save(function(err, locale) {
+      Event.findById(event._id, function(err, data) {
+        data._locale.push(locale._id);
+        data.save();
+        res.redirect('back');
+      });
+    });    
+  });
+
+
+});
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.post('/login', function(req, res) {
+  var post = req.body;
+
+  User.findOne({ 'login': post.login, 'pass': post.password }, function (err, person) {
+    if (!person) return res.redirect('back');
+    req.session.user_id = person._id;
+    req.session.status = person.status;
+    req.session.login = person.login;
+    res.redirect('/auth');
+  });
+});
+
+
+app.get('/logout', function (req, res) {
+  delete req.session.user_id;
+  delete req.session.login;
+  delete req.session.status;
+  res.redirect('back');
+});
+
+
+app.get('/registr', function(req, res) {
+  if (!req.session.user_id)
+    res.render('registr');
+  else
+    res.redirect('/');
+});
+
+
+app.post('/registr', function (req, res) {
+  var post = req.body;
+  var user = new User();
+
+
+  user.login = post.login;
+  user.pass = post.password;
+
+
+  user.save(function(err) {
+    if(err) {throw err;}
+    console.log('New User created');
+    User.findOne({ 'login': post.login, 'pass': post.password }, function (err, person) {
+      req.session.user_id = person._id;
+      req.session.login = person.login;
+      req.session.status = person.status;
+      res.redirect('/login');
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/error', function (req, res) {
   res.render('error');
