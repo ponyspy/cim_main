4b3aed67e8dae88d9c13676f55061ea9161bf613
ponyspy <desade4me@gmail.com>
2013-09-27 18:13:47 +0400

    add folding

--- public/scripts/afisha.js
+++ public/scripts/afisha.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-	$('.premiere').click(function(event) {
-		$('.afisha_description_block:has(.premiere)').slideUp('slow');
+	$('.tag').hover(function() {
+		var tag = '.' + this.className.slice(4);
+
+		$(tag).css('background-color', 'red');
+	}, function() {
+		$('.tag').css('background-color', 'black');
+	});
+
+	$('.tag').click(function(event) {
+		var tag = '.' + this.className.slice(4);
+
+		$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideUp('slow');
 	});
 
 	$('.afisha_date_block').click(function(event) {
-		$('.afisha_description_block:hidden').slideDown('slow');
+		$('.afisha_description_block:hidden, .afisha_item_block:hidden').slideDown('slow');
 	});
 });
