b6a00bfb84a78c81cd70a3bd1eb5b31ca0434cdb
ponyspy <desade4me@gmail.com>
2013-09-29 15:58:56 +0400

    handlers

--- public/scripts/afisha.js
+++ public/scripts/afisha.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
-	$('.tag').hover(function() {
-		var tag = '.' + this.className.slice(4);
 
-		$(tag).css('background-color', 'red');
-	}, function() {
-		$('.tag').css('background-color', 'black');
-	});
+	function setColor(event) {
+		var tag = '.' + this.className.slice(4);
+		$(tag).css('background-color', event.data.color);
+	}
+
+	$('.tag').on('mouseover', {color: 'red'}, setColor);
+	$('.tag').on('mouseout', {color: 'black'}, setColor);
 
 	$('.tag').click(function(event) {
 		var tag = '.' + this.className.slice(4);
+		$(this).data('clicked', !$(this).data('clicked'));
 
-		$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideUp('slow');
-	});
-
-	$('.afisha_date_block').click(function(event) {
-		$('.afisha_description_block:hidden, .afisha_item_block:hidden').slideDown('slow');
+		if ($(this).data('clicked')) {
+			$(tag).off('mouseout');
+			$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideUp('slow');
+		}
+		else {
+			$(tag).css('background-color', 'black');
+			$(tag).on('mouseout', {color: 'black'}, setColor);
+			$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideDown('slow');
+		}
 	});
 });
