880bebd6acfe5d95c79b95f54e6fb1bcac63769a
ponyspy <desade4me@gmail.com>
2013-12-30 17:42:55 +0400

    static

--- public/scripts/static.js
+++ public/scripts/static.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 	$('.nav_item').click(function(event) {
 		$('.content').hide();
 		var index = $(this).index('.nav_item');
-		$('.content').eq(index).show()
+		$('.content').eq(index).show();
+		var right_append = $('.content').eq(index).children('.r_append').html();
+		if (right_append) {
+			$('.right_block').show().html(right_append);
+		}
+		else
+			$('.right_block').hide();
 	});
 });
