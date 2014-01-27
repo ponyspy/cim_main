ea56801c8be882adfa830f9a16daa4e6af718af8
ponyspy <desade4me@gmail.com>
2014-01-27 21:16:10 +0400

    init

--- public/scripts/afisha.js
+++ public/scripts/afisha.js
@@ -1,34 +1,63 @@
 $(document).ready(function() {
 
-	function setColor(event) {
-		var tag = '.' + this.className.slice(4);
-		$(tag).css('background-color', event.data.color);
-	}
+	$('.tag').on({
+		mouseover: function() {
+			var tag = '.' + this.className.slice(4);
+			$(tag).css('background-color', 'red');
+		},
+		mouseout: function() {
+			var tag = '.' + this.className.slice(4);
+			$(tag).removeAttr('style');
+		},
+		click: function() {
+			var tag = '.' + this.className.slice(4);
 
-	$('.tag').on('mouseover', {color: 'red'}, setColor);
-	$('.tag').on('mouseout', {color: 'black'}, setColor);
+			$(tag).data('clicked', !$(tag).data('clicked'));
+			$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideToggle('slow');
+
+			if ($(this).data('clicked')) {
+				$(tag).off('mouseout');
+			}
+			else {
+				$(tag).on('mouseout', function() {
+					$(tag).removeAttr('style');
+				});
+			}
+		}
+	});
+
+
+
+
+
+
+
+
 
 	// $('.afisha_item_block').each(function(index) {
 	//    $(this).is(":visible") ? $(this).height($(this).height()) : $(this).height(0);
 	// });
 
-	$('.tag').click(function(event) {
-		var tag = '.' + this.className.slice(4);
-		$(tag).data('clicked', !$(tag).data('clicked'));
-		$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideToggle('slow');
+	// $('.tag').click(function(event) {
+	// 	var tag = '.' + this.className.slice(4);
+	// 	$(tag).data('clicked', !$(tag).data('clicked'));
+	// 	$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideToggle('slow');
 
-		if ($(this).data('clicked')) {
+		// if ($(this).data('clicked')) {
 			// $(tag).addClass('select_tag');
 			// $(tag).toggleClass('.select_tag');
-			$(tag).off('mouseout');
+			// $(tag).off('mouseout');
 			// $('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideUp('slow');
-		}
-		else {
+		// }
+		// else {
 			// $(tag).removeClass('select_tag');
 			// var color = $(this).css('background-color');
-			$(tag).on('mouseout', {color: 'black'}, setColor);
+			// $(tag).on('mouseout', function() {
+			// 	var tag = '.' + this.className.slice(4);
+			// 	$(tag).removeAttr('style');
+			// });
 			// $('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').filter(':has(.premiere)').slideDown('slow');
 			// $('.afisha_description_block, .afisha_item_block').has(tag).slideDown('slow');
-		}
-	});
+		// }
+	// });
 });
