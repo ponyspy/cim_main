1d0faf3d5ee4aa1a16a494bfd28b75e385050673
ponyspy <desade4me@gmail.com>
2014-02-03 17:53:13 +0400

    cash selectoes

--- public/scripts/afisha.js
+++ public/scripts/afisha.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	// $('.afisha_item_block').css('height', $('.afisha_item_block').height());
+
 	var tags = [];
+	var $sort_items = $('.afisha_description_block, .afisha_item_block');
 
 	$('.tag').on({
 		mouseover: function() {
@@ -13,24 +15,22 @@ $(document).ready(function() {
 		},
 		click: function() {
 			var tag = '.' + this.className.slice(4);
-
 			$(tag).data('clicked', !$(tag).data('clicked'));
-			// $('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideToggle('600');
 
 			if ($(this).data('clicked')) {
 				tags.push(tag);
 
 				$(tag).off('mouseout');
-				$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideUp('600');
+				$sort_items.not(':has(' + tag + ')').slideUp('600');
 			}
 			else {
 				tags.splice(tags.indexOf(tag), 1);
 				var s_tags = tags.join(', ');
 
 				if (tags.length != 0)
-					$('.afisha_description_block, .afisha_item_block').has(s_tags).not(':has(' + tag + ')').slideDown('600');
+					$sort_items.has(s_tags).not(':has(' + tag + ')').slideDown('600');
 				else
-					$('.afisha_description_block, .afisha_item_block').not(':has(' + tag + ')').slideDown('600');
+					$sort_items.not(':has(' + tag + ')').slideDown('600');
 
 				$(tag).on('mouseout', function() {
 					$(tag).removeAttr('style');
