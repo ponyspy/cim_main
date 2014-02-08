a7f99a7e9c913e2f00b9642499a748ff41e5df25
ponyspy <desade4me@gmail.com>
2014-02-08 14:14:26 +0400

    fix maps

--- public/scripts/g_maps.js
+++ public/scripts/g_maps.js
@@ -1,119 +1,119 @@
 var map;
 var style_array =
 [
-    {
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.business",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.business",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.place_of_worship",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.place_of_worship",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "saturation": 50
-            },
-            {
-                "gamma": 0
-            },
-            {
-                "hue": "#50a5d1"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.neighborhood",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#333333"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "weight": 0.5
-            },
-            {
-                "color": "#333333"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.station",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "gamma": 1
-            },
-            {
-                "saturation": 50
-            }
-        ]
-    }
+	{
+		"stylers": [
+				{
+						"saturation": -100
+				},
+				{
+						"gamma": 1
+				}
+		]
+	},
+	{
+		"elementType": "labels.text.stroke",
+		"stylers": [
+				{
+						"visibility": "off"
+				}
+		]
+	},
+	{
+		"featureType": "poi.business",
+		"elementType": "labels.text",
+		"stylers": [
+				{
+						"visibility": "off"
+				}
+		]
+	},
+	{
+		"featureType": "poi.business",
+		"elementType": "labels.icon",
+		"stylers": [
+				{
+						"visibility": "off"
+				}
+		]
+	},
+	{
+		"featureType": "poi.place_of_worship",
+		"elementType": "labels.text",
+		"stylers": [
+				{
+						"visibility": "off"
+				}
+		]
+	},
+	{
+		"featureType": "poi.place_of_worship",
+		"elementType": "labels.icon",
+		"stylers": [
+				{
+						"visibility": "off"
+				}
+		]
+	},
+	{
+		"featureType": "road",
+		"elementType": "geometry",
+		"stylers": [
+				{
+						"visibility": "simplified"
+				}
+		]
+	},
+	{
+		"featureType": "water",
+		"stylers": [
+				{
+						"visibility": "on"
+				},
+				{
+						"saturation": 50
+				},
+				{
+						"gamma": 0
+				},
+				{
+						"hue": "#50a5d1"
+				}
+		]
+	},
+	{
+		"featureType": "administrative.neighborhood",
+		"elementType": "labels.text.fill",
+		"stylers": [
+				{
+						"color": "#333333"
+				}
+		]
+	},
+	{
+		"featureType": "road.local",
+		"elementType": "labels.text",
+		"stylers": [
+				{
+						"weight": 0.5
+				},
+				{
+						"color": "#333333"
+				}
+		]
+	},
+	{
+		"featureType": "transit.station",
+		"elementType": "labels.icon",
+		"stylers": [
+				{
+						"gamma": 1
+				},
+				{
+						"saturation": 50
+				}
+		]
+	}
 ]
 
 function initialize() {
@@ -123,11 +123,11 @@ function initialize() {
 		zoom: 18,
 		center: myLatlng,
 		styles: style_array,
-    zoomControl: true,
-    zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.LARGE,
-        position: google.maps.ControlPosition.LEFT_CENTER
-    }
+		zoomControl: true,
+		zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.LARGE,
+				position: google.maps.ControlPosition.LEFT_CENTER
+		}
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'),
