9f7a54ca4c6d948abea5d4db020e5a451b0d449d
ponyspy <desade4me@gmail.com>
2014-05-25 13:17:33 +0400

    drop

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -100,28 +100,46 @@ $(document).ready(function() {
 		});
 	});
 
-	// $('.image_upload').mfupload({
+	$('.dropzone').filedrop({
+		url: '/upload',
+		paramname: 'photo',
+		fallback_id: 'upload_button',
+		allowedfiletypes: ['image/jpeg','image/png','image/gif'],
+		allowedfileextensions: ['.jpg','.jpeg','.png','.gif'],
+		maxfiles: 5,
+		maxfilesize: 8,
+		dragOver: function() {
+			$(this).css('background-color', '#c9a2ae');
+		},
+		dragLeave: function() {
+			$(this).removeAttr('style');
+		},
+		uploadStarted: function(i, file, len) {
+			var photo = $('<div/>', {'class':'photo'});
+			var scale = $('<div/>', {'class':'scale'});
 
-	// 	type		: 'jpg,png,tif,jpeg',
-	// 	maxsize		: 6,
-	// 	post_upload	: '/edit',
-	// 	folder		: '',
-	// 	ini_text	: 'Нажми или перетащи',
-	// 	over_text	: 'Отпускай!',
-	// 	over_col	: '',
-	// 	over_bkcol	: 'white',
+			photo.append(scale);
+			$('.photos').prepend(photo);
+		},
+		uploadFinished: function(i, file, response, time) {
+			$('.photo').eq(i).attr('style', 'background-image:url(' + response + ')');
+		},
+		progressUpdated: function(i, file, progress) {
+			$('.photo').eq(i).children('.scale').text(progress + '%').css('width', progress + '%');
+		},
+		afterAll: function() {
+			$('.dropzone').removeAttr('style');
+		}
+	});
 
-	// 	init		: function(){ },
-	// 	start		: function(result){ },
-	// 	loaded		: function(result) {
-	// 		$('.image_upload').css('background-image', 'url(' + result.path + ')');
-	// 		img_preview = result.path;
-	// 	},
-	// 	progress	: function(result){ },
-	// 	error		: function(error){ },
-	// 	completed	: function(){ }
+	$(document).on('dblclick', '.photo', function() {
+		var path = $(this).attr('style').slice(21,-1);
+		var ph = $(this);
 
-	// });
+		$.post('/photo_remove', {path: path}).done(function(data) {
+			ph.remove();
+		});
+	});
 
 
 	$(document).on('keyup change', '.m_search', function(event) {
