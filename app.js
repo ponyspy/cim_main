c8d371d3e5bc05d5016bae0b457c2333bc7d1de9
ponyspy <desade4me@gmail.com>
2014-05-30 15:16:18 +0400

    schema

--- app.js
+++ app.js
@@ -1421,13 +1421,11 @@ app.get('/schema', function (req, res) {
 
       fs.mkdir(__dirname + '/public/images/events/' + event._id + '/photos', function() {
         fs.rename(__dirname + '/public/images/events/' + event._id + '/photo.jpg', __dirname + '/public/images/events/' + event._id + '/photos/' + name + '.jpg');
-
+        event.photo = '/images/events/' + event._id + '/photos/' + name + '.jpg';
+        event.save(function() {
+          callback();
+        });
       });
-
-      event.photo = '/images/events/' + event._id + '/photos/' + name + '.jpg';
-      event.save();
-
-      callback();
 
     }, function() {
       var date = new Date();
