bb91b773f6eda68b6e792ae885c6eb22f4bca371
ponyspy <desade4me@gmail.com>
2015-02-15 23:16:50 +0300

    dt

--- public/scripts/afisha.js
+++ public/scripts/afisha.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 	var interval_end = $('.afisha_actual_block').attr('interval_end');
 
 	$.ajax({
-		dataType: true,
+		dataType: 'json',
 	   url: 'https://tickets.meyerhold.ru/api/?secret=' + secret + '&d1=' + interval_start + '&d2=' + interval_end,
 	   xhrFields: {
 	      withCredentials: true
