03ad3346031855671ca3b1e128e9b6c828079b62
ponyspy <desade4me@gmail.com>
2015-03-10 16:19:09 +0300

    error handle

--- app.js
+++ app.js
@@ -38,35 +38,35 @@ app.use(function(req, res, next) {
 app.use(app.router);
 
 
-// app.use(function(req, res, next) {
-//   res.status(404);
+app.use(function(req, res, next) {
+  res.status(404);
 
-//   // respond with html page
-//   if (req.accepts('html')) {
-//     res.render('error', { url: req.url, status: 404 });
-//     return;
-//   }
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('error', { url: req.url, status: 404 });
+    return;
+  }
 
-//   // respond with json
-//   if (req.accepts('json')) {
-//       res.send({
-//       error: {
-//         status: 'Not found'
-//       }
-//     });
-//     return;
-//   }
+  // respond with json
+  if (req.accepts('json')) {
+      res.send({
+      error: {
+        status: 'Not found'
+      }
+    });
+    return;
+  }
 
-//   // default to plain-text
-//   res.type('txt').send('Not found');
-// });
+  // default to plain-text
+  res.type('txt').send('Not found');
+});
 
-// app.use(function(err, req, res, next) {
-//   var status = err.status || 500;
+app.use(function(err, req, res, next) {
+  var status = err.status || 500;
 
-//   res.status(status);
-//   res.render('error', { error: err, status: status });
-// });
+  res.status(status);
+  res.render('error', { error: err, status: status });
+});
 
 
 // -------------------
